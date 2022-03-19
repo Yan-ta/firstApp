b2cf0188f0734eacd66d6ad4b663380f00a434be
Yan-ta <yatashe@yandex.ru>
2022-03-19 20:15:01 +0300

    logicButton

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var HelloWorldLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        HelloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func makeButtonAction() {
+        if HelloWorldLabel.isHidden {
+            HelloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            HelloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+    }
 }
 
